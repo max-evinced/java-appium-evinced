91345b34535bc87a900bddccc2f984666880aabd
Max Dobeck <max.dobeck@evinced.com>
2024-06-14 14:42:43 -0400

    first commit, wip

---
+++ src/main/java/com/maxdobeck/evinced/appium/App.java
@@ -0,0 +1,13 @@
+package com.maxdobeck.evinced.appium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
